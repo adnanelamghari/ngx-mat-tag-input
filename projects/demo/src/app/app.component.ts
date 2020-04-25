a081718622c10fa76b5565aad2523deb214ab4cc
Adnane Lamghari <adnane.lmg@gmail.com>
2020-04-25 23:05:55 +0000

    add indigo-pink theme to lib demo

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public autoCompleteTags = ['Angular', 'React', 'VueJs'];
+  public autoCompleteTags = ['Angular', 'React', 'VueJs', 'Meteor', 'Ember.js', 'Aurelia', 'Backbone.js'];
   public autoCompleteTagsObjects = [{name: 'Angular', id: 0}, {name: 'React', id: 1}, {name: 'VueJs', id: 2}];
 
   change($event) {
